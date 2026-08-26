cc6452727ea8abe62d9df8e35ee3df6561c4a73b
jungminn <jungminn@hanmail.net>
2026-08-26 13:23:55 -0400

    feat: add request validation (#14)

--- src/main/java/com/travelplanner/dto/SignupRequest.java
+++ src/main/java/com/travelplanner/dto/SignupRequest.java
@@ -2,17 +2,20 @@ package com.travelplanner.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public class SignupRequest {
 
-    @NotBlank
+    @NotBlank(message = "Name is required")
+    @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
     private String name;
 
-    @NotBlank
-    @Email
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
 
-    @NotBlank
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, max = 100, message = "Password must be at least 8 characters")
     private String password;
 
     public String getName() {
